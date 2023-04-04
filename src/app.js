1dd95d00464bf114f1e0782eab0f94ecae24e847
atbranco <ateresabranco@gmail.com>
2023-04-04 19:58:25 +0100

    Add current location functionality

--- src/app.js
+++ src/app.js
@@ -23,8 +23,13 @@ function updateTime() {
     );
   }
 }
+setInterval(updateTime, 1000);
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -39,7 +44,6 @@ function updateCity(event) {
           )}</div>
         </div>`;
 }
-setInterval(updateTime, 1000);
 
 let citiesSelect = document.querySelector("#city");
 citiesSelect.addEventListener("change", updateCity);
