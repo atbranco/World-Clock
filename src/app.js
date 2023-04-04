94bbc0e291786d374d1a296948805860ee4a2b6b
atbranco <ateresabranco@gmail.com>
2023-04-04 19:12:20 +0100

    Added current date and time to the project

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+//Los Angeles
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = document.querySelector(".date");
+  let losAngelesTimeElement = document.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = document.querySelector(".date");
+  let parisTimeElement = document.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss[<small>]A[</small>]");
+}
 
+setInterval(updateTime, 1000);
